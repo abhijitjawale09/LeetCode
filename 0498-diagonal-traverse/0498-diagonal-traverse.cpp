d4c54811d4399e63fc8a3ae1733b59a6782cda8c
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2025-12-09 16:34:22 +0530

    Time: 2 ms (52.57%), Space: 22.8 MB (51.16%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -4,24 +4,65 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<int> res;
+        vector<int> ans;
+        int i = 0;
+        int j = 0;
+        bool upward = true;
+        while(ans.size() < n * m) {
+             ans.push_back(mat[i][j]);
 
-        int i = 0, j = 0;
-        for(int s = 0; s <= n + m -2; s++) {
-            for(int x = 0; x <= s; x++) {
-                int i = x;
-                int j = s - i;
+             if(upward) {
+                if(j == m-1) {
+                    i++;
+                    upward = false;
+                }else if(i == 0) {
+                    j++;
+                    upward = false;
+                }else{
+                    i-- , j++;
 
-                if(s%2 == 0) {
-                    swap(i , j);
                 }
-                if(i >= n || j >= m) {
-                    continue;
+            }else{
+                if(i == n - 1){
+                    j++;
+                    upward = true;
+                }else if(j == 0){
+                    i++;
+                    upward = true;
+                }else{
+                    i++;
+                    j--;
                 }
-
-                res.push_back(mat[i][j]);
-            }
+             }
         }
-        return  res;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+//         int n = mat.size();
+//         int m = mat[0].size();
+
+//         vector<int> res;
+
+//         int i = 0, j = 0;
+//         for(int s = 0; s <= n + m -2; s++) {
+//             for(int x = 0; x <= s; x++) {
+//                 int i = x;
+//                 int j = s - i;
+
+//                 if(s%2 == 0) {
+//                     swap(i , j);
+//                 }
+//                 if(i >= n || j >= m) {
+//                     continue;
+//                 }
+
+//                 res.push_back(mat[i][j]);
+//             }
+//         }
+//         return  res;
+//     }
+// };

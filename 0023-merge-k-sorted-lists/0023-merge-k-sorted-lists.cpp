69b3566b63e14f7bba68eec48055f4b70e44382e
abhijitjawale09 <150526760+abhijitjawale09@users.noreply.github.com>
2026-01-13 14:01:18 +0530

    Time: 0 ms (100%), Space: 18.6 MB (38.8%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,40 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
+    ListNode* mergetwolists(ListNode* l1 , ListNode* l2) {
+        if(!l1) {
+            return l2;
+        }
+        if(!l2) {
+            return l1;
         }
 
-        while (lists.size() > 1) {
-            vector<ListNode*> temp;
-            for (size_t i = 0; i < lists.size(); i += 2) {
-                ListNode* l1 = lists[i];
-                ListNode* l2 = i + 1 < lists.size() ? lists[i + 1] : nullptr;
-                temp.push_back(mergeLists(l1, l2));
-            }
-            lists = move(temp);
+        if(l1->val <= l2->val) {
+            l1->next = mergetwolists(l1->next, l2);
+            return l1;
+        }else{
+            l2->next = mergetwolists(l1 , l2->next);
+            return l2;
         }
-
-        return lists[0];        
+        return NULL;
     }
-
-private:
-    ListNode* mergeLists(ListNode* l1, ListNode* l2) {
-        ListNode dummy;
-        ListNode* node = &dummy;
-
-        while (l1 && l2) {
-            if (l1->val > l2->val) {
-                node->next = l2;
-                l2 = l2->next;
-            } else {
-                node->next = l1;
-                l1 = l1->next;
-            }
-            node = node->next;
+    ListNode* partitionAndmerge(int start , int end , vector<ListNode*>& lists) {
+        if(start == end) {
+            return lists[start];
         }
+        if(start > end) {
+            return NULL;
+        }
+        int mid = start + (end - start) / 2;
 
-        node->next = l1 ? l1 : l2;
-        return dummy.next;
-    }    
+        ListNode* l1 = partitionAndmerge(start , mid , lists);
+        ListNode* l2 = partitionAndmerge(mid + 1 , end , lists);
+
+        return mergetwolists(l1 , l2);
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if(n == 0) {
+            return NULL;
+        }
+        if(n == 1) {
+            return lists[0];
+        }
+        
+        return partitionAndmerge(0 , n - 1, lists);
+    }
 };

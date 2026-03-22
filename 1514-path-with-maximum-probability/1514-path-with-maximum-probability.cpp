98d0a8356950666fc7585b9e72d5face2b4b0978
Abhijit Jawale <150526760+abhijitjawale09@users.noreply.github.com>
2026-03-23 00:36:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
+    typedef pair<double , int> P;
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        vector<double> maxProb(n , 0.0);
-        maxProb[start_node] = 1.0;
+        unordered_map<int , vector<pair<int ,double>>> adj;
 
-        for(int i = 0; i < n - 1; i++) {
-            bool update = false;
-            for(int j = 0; j < edges.size(); j++) {
-                int u = edges[j][0];
-                int v = edges[j][1];
-                double prob = succProb[j];
+        vector<double> res(n , 0);
 
-                if(maxProb[u] * prob > maxProb[v]) {
-                    maxProb[v] = maxProb[u] * prob;
-                    update = true;
-                }
-                 if (maxProb[v] * prob > maxProb[u]) {
-                    maxProb[u] = maxProb[v] * prob;
-                    update = true;
-                }
-
-            }
-            if(!update) break;
+        for(int i = 0; i < edges.size(); i++) {
+            adj[edges[i][0]].push_back({edges[i][1] , (double)succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0] ,(double)succProb[i]});
         }
 
-        return maxProb[end_node];
+        priority_queue<P> pq;
+        res[start_node] = 1;
+        pq.push({1.0 , start_node});
+
+
+        while(!pq.empty()) {
+            int currnode = pq.top().second;
+            double currprob = pq.top().first;
+            pq.pop();
+            for(auto &temp : adj[currnode]) {
+                double adjprob = temp.second;
+                int adjnode = temp.first;
+
+                if(res[adjnode] < currprob*adjprob ){
+                    res[adjnode] = currprob*adjprob;
+                    pq.push({res[adjnode] , adjnode});
+                }
+            }
+        }
+
+        return res[end_node];
     }
 };

b28c97bc2069b8ecce789646417b700884f249b9
Abhijit Jawale <150526760+abhijitjawale09@users.noreply.github.com>
2026-04-17 14:29:53 +0530

    Time: 135 ms (73.68%), Space: 134 MB (61.4%) - LeetHub

---
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        int n = creators.size();
+        vector<vector<string>> res;
+
+        unordered_map<string , long long> mp;
+        unordered_map<string , pair<int , string>> imp;
+        long long mx  = -1;
+        for(int i = 0; i < n; i++) {
+            long long v = views[i];
+            if(mp.find(creators[i]) == mp.end()) {
+                mp[creators[i]] = v;
+            }else{
+                mp[creators[i]] += v;
+            }
+
+            if(mx < mp[creators[i]]) mx = mp[creators[i]];
+
+            if(imp.find(creators[i]) == imp.end()) {
+                imp[creators[i]] = {views[i] , ids[i]};
+            }else{
+                if(imp[creators[i]].first < views[i]) {
+                    imp[creators[i]] = {views[i] , ids[i]};
+                }
+                else if(imp[creators[i]].first == views[i] && imp[creators[i]].second > ids[i]) {
+                    imp[creators[i]] = {views[i] , ids[i]};
+                }
+            }
+        }
+
+        for(auto m : mp)  {
+            if(mx == m.second) {
+                res.push_back({m.first , imp[m.first].second});
+            }
+        }
+        return res;
+    }
+};

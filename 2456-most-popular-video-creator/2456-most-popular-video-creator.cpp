033ff25f6a2ba11bd24689cfc51f9aa7ff6279cd
Abhijit Jawale <150526760+abhijitjawale09@users.noreply.github.com>
2026-04-17 14:45:28 +0530

    Time: 154 ms (62.28%), Space: 134.2 MB (49.12%) - LeetHub

--- 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
         int n = creators.size();
-        vector<vector<string>> res;
 
         unordered_map<string , long long> mp;
-        unordered_map<string , pair<int , string>> imp;
-        long long mx  = -1;
+        unordered_map<string , pair<long long , string>> imp;
+        long long mx = -1;
         for(int i = 0; i < n; i++) {
-            long long v = views[i];
+
+            int v = views[i];
             if(mp.find(creators[i]) == mp.end()) {
                 mp[creators[i]] = v;
             }else{
                 mp[creators[i]] += v;
             }
 
-            if(mx < mp[creators[i]]) mx = mp[creators[i]];
+
+            if(mx < mp[creators[i]]) {
+                mx = mp[creators[i]];
+            }
 
             if(imp.find(creators[i]) == imp.end()) {
-                imp[creators[i]] = {views[i] , ids[i]};
+                imp[creators[i]] = { v , ids[i]};
             }else{
-                if(imp[creators[i]].first < views[i]) {
-                    imp[creators[i]] = {views[i] , ids[i]};
-                }
-                else if(imp[creators[i]].first == views[i] && imp[creators[i]].second > ids[i]) {
-                    imp[creators[i]] = {views[i] , ids[i]};
+
+                if(imp[creators[i]].first < v){
+                    imp[creators[i]] = {v , ids[i]};
+                }else if(imp[creators[i]].first == v &&  imp[creators[i]].second > ids[i]) {
+                     imp[creators[i]] = {v , ids[i]};
                 }
             }
+
         }
 
-        for(auto m : mp)  {
+        vector<vector<string>> res;
+        for(auto m : mp) {
             if(mx == m.second) {
                 res.push_back({m.first , imp[m.first].second});
             }
@@ -37,3 +42,46 @@ public:
         return res;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+//         int n = creators.size();
+//         vector<vector<string>> res;
+
+//         unordered_map<string , long long> mp;
+//         unordered_map<string , pair<int , string>> imp;
+//         long long mx  = -1;
+//         for(int i = 0; i < n; i++) {
+//             long long v = views[i];
+//             if(mp.find(creators[i]) == mp.end()) {
+//                 mp[creators[i]] = v;
+//             }else{
+//                 mp[creators[i]] += v;
+//             }
+
+//             if(mx < mp[creators[i]]) mx = mp[creators[i]];
+
+//             if(imp.find(creators[i]) == imp.end()) {
+//                 imp[creators[i]] = {views[i] , ids[i]};
+//             }else{
+//                 if(imp[creators[i]].first < views[i]) {
+//                     imp[creators[i]] = {views[i] , ids[i]};
+//                 }
+//                 else if(imp[creators[i]].first == views[i] && imp[creators[i]].second > ids[i]) {
+//                     imp[creators[i]] = {views[i] , ids[i]};
+//                 }
+//             }
+//         }
+
+//         for(auto m : mp)  {
+//             if(mx == m.second) {
+//                 res.push_back({m.first , imp[m.first].second});
+//             }
+//         }
+//         return res;
+//     }
+// };

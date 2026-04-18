6bbd4e449a05063fd35ee3452ca8faa7ee2923bd
Abhijit Jawale <150526760+abhijitjawale09@users.noreply.github.com>
2026-04-18 10:11:30 +0530

    Time: 399 ms (34.52%), Space: 228.9 MB (25.04%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int n;
+    int len = 1;
+    int solve(unordered_map<int , vector<int>>& adj , int st , string& s) {        
+
+        int logest =  0 , secondlogest = 0;
+
+        for(auto &child : adj[st]) {
+            int childlen = solve(adj ,child, s);
+
+            if(s[child] == s[st]) {
+                continue;
+            }
+
+            if(childlen > logest) {
+                secondlogest = logest;
+                logest = childlen;
+            }else if(childlen > secondlogest) {
+                secondlogest = childlen;
+            }
+        }
+        len = max(len , logest + secondlogest + 1);
+        return logest + 1;
+    }
+    int longestPath(vector<int>& parent, string s) {
+        n = parent.size();
+        unordered_map<int , vector<int>> adj;
+
+        for(int i = 0; i < n; i++) {
+            int par = parent[i];
+            adj[par].push_back(i);
+        }
+
+        solve(adj , 0, s);
+        return len;
+    }
+};
